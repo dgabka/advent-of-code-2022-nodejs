cce1a6522042fa3f9fb7d99de4aef4898fa8659d
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-04 03:21:48 +0100

    complete day 3

---
+++ day3/part1.js
@@ -0,0 +1,36 @@
+const getInput = require("../utils/getInput");
+
+function splitInHalf(str) {
+  return [str.slice(0, str.length / 2), str.slice(str.length / 2)];
+}
+
+const ASCII_LOWERCASE_RANGE_START = 97;
+const ASCII_PRIORITY_LOWERCASE_OFFSET = 96;
+const ASCII_PRIORITY_UPPERCASE_OFFSET = 38;
+
+function getPriorityForItem(item) {
+  const ascii = item.charCodeAt(0);
+  if (ascii >= ASCII_LOWERCASE_RANGE_START) {
+    return ascii - ASCII_PRIORITY_LOWERCASE_OFFSET;
+  } else {
+    return ascii - ASCII_PRIORITY_UPPERCASE_OFFSET;
+  }
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const rucksacks = input.split("\n");
+
+  const answer = rucksacks.reduce((previous, current) => {
+    const [compartmentA, compartmentB] = splitInHalf(current);
+    const item = compartmentA
+      .split("")
+      .find((letter) => compartmentB.includes(letter));
+    return previous + getPriorityForItem(item);
+  }, 0);
+
+  console.log(answer);
+}
+
+main();
---
+++ day3/part2.js
@@ -0,0 +1,34 @@
+const getInput = require("../utils/getInput");
+
+const ASCII_LOWERCASE_RANGE_START = 97;
+const ASCII_PRIORITY_LOWERCASE_OFFSET = 96;
+const ASCII_PRIORITY_UPPERCASE_OFFSET = 38;
+
+function getPriorityForItem(item) {
+  const ascii = item.charCodeAt(0);
+  if (ascii >= ASCII_LOWERCASE_RANGE_START) {
+    return ascii - ASCII_PRIORITY_LOWERCASE_OFFSET;
+  } else {
+    return ascii - ASCII_PRIORITY_UPPERCASE_OFFSET;
+  }
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const groups = input
+    .match(/(?:^\w*$\n?){3}/gm) // split into groups of three
+    .map((s) => s.match(/(?:^\w+$)/gm)); // split each group
+
+  const answer = groups.reduce((sum, group) => {
+    const uniqueItems = new Set(group.join());
+    const badge = Array.from(uniqueItems.values()).find((item) =>
+      group.every((rucksack) => rucksack.includes(item))
+    );
+    return sum + getPriorityForItem(badge);
+  }, 0);
+
+  console.log(answer);
+}
+
+main();
