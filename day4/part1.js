f47d9cf6eccff1ed1882a37f362b5f8878a07986
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-05 13:59:27 +0100

    complete day 4

---
+++ day4/part1.js
@@ -0,0 +1,24 @@
+const getInput = require("../utils/getInput");
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const pairs = input
+    .split("\n")
+    .map((pair) => pair.split(/-|,/g).map((id) => parseInt(id)));
+
+  const answer = pairs.reduce((sum, ids) => {
+    const [startA, endA, startB, endB] = ids;
+    if (
+      (startA <= startB && endA >= endB) ||
+      (startB <= startA && endB >= endA)
+    ) {
+      return sum + 1;
+    }
+    return sum;
+  }, 0);
+
+  console.log(answer);
+}
+
+main();
