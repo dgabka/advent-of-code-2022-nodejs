f185bdf0a7d06ecf9c15b006cd1a7afe8b182ae8
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-05 15:23:04 +0100

    complete day 5

---
+++ day5/part2.js
@@ -0,0 +1,33 @@
+const getInput = require("../utils/getInput");
+
+function transpose(matrix) {
+  return matrix.reduceRight(
+    (prev, next) => next.map((item, i) => (prev[i] || []).concat(item)),
+    []
+  );
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const inputLines = input.split("\n");
+  const crates = transpose(
+    inputLines.slice(0, 8).map((line) =>
+      line
+        .match(/(\s{3}\s?|\[\w\]\s?)/g)
+        .flatMap((crate) => crate.match(/\w|\s+/))
+        .map((x) => x.trim())
+    )
+  ).map((stack) => stack.filter(Boolean));
+  const moves = inputLines.slice(10, -1).map((move) => move.match(/(\d+)/g));
+
+  moves.forEach(([move, from, to]) => {
+    crates[to - 1].push(...crates[from - 1].splice(-move));
+  });
+
+  const answer = crates.map((stack) => stack[stack.length - 1]).join("");
+
+  console.log(answer);
+}
+
+main();
