74a47ab6ddf4b824955ec1d0f750d49b2b897418
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-09 23:57:49 +0100

    complete day 7

---
+++ day7/part1.ts
@@ -0,0 +1,71 @@
+import getInput from "../utils/getInput";
+
+class TreeNode {
+  name: string;
+  size: number;
+  files: number[];
+  dirs: TreeNode[];
+  parent: TreeNode | null;
+
+  constructor(name: string, parent: TreeNode) {
+    this.name = name;
+    this.size = 0;
+    this.files = [];
+    this.dirs = [];
+    this.parent = parent;
+  }
+}
+
+let root: TreeNode = {
+  name: "/",
+  size: 0,
+  files: [],
+  dirs: [],
+  parent: null,
+};
+
+function sumSize(node: TreeNode): number {
+  if (node.size < 100000) {
+    return node.size + node.dirs.reduce((s, dir) => s + sumSize(dir), 0);
+  }
+  return node.dirs.reduce((s, dir) => s + sumSize(dir), 0);
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+  const output = input.trim().split("\n").slice(2);
+
+  let currentNode = root;
+
+  output.forEach((line) => {
+    if (/^dir.*/.test(line)) {
+      const [_, name] = line.split(" ");
+      if (name) {
+        currentNode.dirs.push(new TreeNode(name, currentNode));
+      }
+    } else if (/^\d+/.test(line)) {
+      const [size] = line.split(" ");
+      if (size) {
+        currentNode.size += parseInt(size);
+        let tmp = currentNode.parent;
+        while (tmp !== null) {
+          tmp.size += parseInt(size);
+          tmp = tmp.parent;
+        }
+      }
+    } else if (/^\$ cd/.test(line)) {
+      const [_, __, dir] = line.split(" ");
+      if (dir === "..") {
+        currentNode = currentNode.parent as TreeNode;
+      } else {
+        currentNode = currentNode.dirs.find(({ name }) => name === dir)!;
+      }
+    }
+  });
+
+  let answer = sumSize(root);
+
+  console.log(answer);
+}
+
+main();
---
+++ day7/part2.ts
@@ -0,0 +1,94 @@
+import getInput from "../utils/getInput";
+
+class TreeNode {
+  name: string;
+  size: number;
+  files: number[];
+  dirs: TreeNode[];
+  parent: TreeNode | null;
+
+  constructor(name: string, parent: TreeNode) {
+    this.name = name;
+    this.size = 0;
+    this.files = [];
+    this.dirs = [];
+    this.parent = parent;
+  }
+}
+
+let root: TreeNode = {
+  name: "/",
+  size: 0,
+  files: [],
+  dirs: [],
+  parent: null,
+};
+
+function findDirToDelete(
+  node: TreeNode,
+  sizeToFree: number,
+  dirSizeToDelete: number
+): number {
+  if (node.dirs.length === 0) {
+    if (node.size >= sizeToFree) {
+      return Math.min(node.size, dirSizeToDelete);
+    }
+    return dirSizeToDelete;
+  }
+
+  if (node.size < sizeToFree) {
+    return dirSizeToDelete;
+  }
+
+  return node.dirs.reduce((n, dir) => {
+    const x = findDirToDelete(dir, sizeToFree, n);
+    if (node.size > sizeToFree) {
+      return Math.min(x, n, node.size);
+    }
+    return Math.min(x, n);
+  }, dirSizeToDelete);
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+  const output = input.trim().split("\n").slice(2);
+
+  let currentNode = root;
+
+  output.forEach((line) => {
+    if (/^dir.*/.test(line)) {
+      const [_, name] = line.split(" ");
+      if (name) {
+        currentNode.dirs.push(new TreeNode(name, currentNode));
+      }
+    } else if (/^\d+/.test(line)) {
+      const [size] = line.split(" ");
+      if (size) {
+        currentNode.size += parseInt(size);
+        let tmp = currentNode.parent;
+        while (tmp !== null) {
+          tmp.size += parseInt(size);
+          tmp = tmp.parent;
+        }
+      }
+    } else if (/^\$ cd/.test(line)) {
+      const [_, __, dir] = line.split(" ");
+      if (dir === "..") {
+        currentNode = currentNode.parent as TreeNode;
+      } else {
+        currentNode = currentNode.dirs.find(({ name }) => name === dir)!;
+      }
+    }
+  });
+
+  const totalSpace = 70000000;
+  const freeSpaceNeeded = 30000000;
+  const occupiedSpace = root.size;
+  const sizeToFree = freeSpaceNeeded - (totalSpace - occupiedSpace);
+
+  const answer = findDirToDelete(root, sizeToFree, root.size);
+
+  console.log(answer);
+}
+
+main();
