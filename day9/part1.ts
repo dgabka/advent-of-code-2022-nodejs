07d76d209109f1e13dee4ea2da359f06fed1283d
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-11 02:02:49 +0100

    complete day 9

---
+++ day9/part1.ts
@@ -0,0 +1,54 @@
+import getInput from "../utils/getInput";
+
+type coords = { x: number; y: number };
+
+function getNewTailPosition(head: coords, tail: coords): coords {
+  const xDiff = Math.abs(head.x - tail.x);
+  const yDiff = Math.abs(head.y - tail.y);
+  if (xDiff > 1 || yDiff > 1) {
+    tail.x = tail.x + Math.sign(head.x - tail.x);
+    tail.y = tail.y + Math.sign(head.y - tail.y);
+  }
+
+  return tail;
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const moves = input
+    .trim()
+    .split("\n")
+    .map((move) => move.split(" "));
+
+  const head: coords = { x: 0, y: 0 };
+  let tail: coords = { x: 0, y: 0 };
+
+  const moveHistory = new Set([JSON.stringify(tail)]);
+
+  moves.forEach(([direction, steps]) => {
+    for (let i = 0; i < parseInt(steps); i++) {
+      switch (direction) {
+        case "D":
+          head.y--;
+          break;
+        case "U":
+          head.y++;
+          break;
+        case "L":
+          head.x--;
+          break;
+        case "R":
+          head.x++;
+          break;
+      }
+
+      getNewTailPosition(head, tail);
+      moveHistory.add(JSON.stringify(tail));
+    }
+  });
+
+  console.log(moveHistory.size);
+}
+
+main();
---
+++ day9/part2.ts
@@ -0,0 +1,53 @@
+import getInput from "../utils/getInput";
+
+type coords = { x: number; y: number };
+
+function getNewKnotPosition(prevKnot: coords, knot: coords): coords {
+  const xDiff = Math.abs(prevKnot.x - knot.x);
+  const yDiff = Math.abs(prevKnot.y - knot.y);
+  if (xDiff > 1 || yDiff > 1) {
+    knot.x = knot.x + Math.sign(prevKnot.x - knot.x);
+    knot.y = knot.y + Math.sign(prevKnot.y - knot.y);
+  }
+
+  return knot;
+}
+
+async function main() {
+  const input = await getInput(__dirname);
+
+  const moves = input
+    .trim()
+    .split("\n")
+    .map((move) => move.split(" "));
+
+  const head: coords = { x: 0, y: 0 };
+  const knots: Array<coords> = new Array(9).fill(0).map(() => ({ ...head }));
+
+  const moveHistory = new Set([JSON.stringify(knots[8])]);
+
+  moves.forEach(([direction, steps]) => {
+    for (let i = 0; i < parseInt(steps); i++) {
+      switch (direction) {
+        case "D":
+          head.y--;
+          break;
+        case "U":
+          head.y++;
+          break;
+        case "L":
+          head.x--;
+          break;
+        case "R":
+          head.x++;
+      }
+
+      knots.reduce(getNewKnotPosition, head);
+      moveHistory.add(JSON.stringify(knots[8]));
+    }
+  });
+
+  console.log(moveHistory.size);
+}
+
+main();
